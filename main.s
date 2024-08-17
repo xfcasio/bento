b15800bd5118a7a0b2e0f4b36e5ff387ac228a51
xfcasio <xfc1@pm.me>
2024-08-17 19:26:56 +0300

    add variations for iter macro for nesting, and fix puts macro

--- main.s
+++ main.s
@@ -3,10 +3,17 @@ include 'bento.inc'
 
 segment executable readable
 entry $
-  @iter r15, 1, [nums.len]
-    write STDOUT, nums, r15
+  @iter0 r8, 0, 1
+    @iter1 rax, 0, 1
+      @iter2 rbx, 0, 1
+        mov qword [dummy], rbx
+        add qword [dummy], 48
+        write STDOUT, dummy, 1
+      @done2
+      write STDOUT, newline, 1
+    @done1
     write STDOUT, newline, 1
-  @done
+  @done0
 
   puts hello_world
   exit 0
@@ -15,4 +22,5 @@ entry $
 segment readable writeable 
 @mkstr hello_world, <"Hello, World", 0>
 @mkstr nums, "123456789"
+dummy: dq 0
 newline: db 0xA
